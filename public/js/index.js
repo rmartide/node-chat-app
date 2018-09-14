dae2c0c3c0d355110f161ce9266adaffff517ab3
Rodrigo Martín de Lamo <rmartin@devcenter.es>
2018-09-14 10:49:17 +0200

    Broadcasting newMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'jen',
-        text: 'Hey. This is Andrew.'
-    })
 });
 
 socket.on('disconnect', function () {
