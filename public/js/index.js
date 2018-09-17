2f4c79af46ea36d1779ae891f62d864bc7f164c7
Rodrigo Martín de Lamo <rmartin@devcenter.es>
2018-09-17 10:48:20 +0200

    Add scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,21 @@
 var socket = io();
 var locationButton;
 
+function scrollToBottom() {
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight= messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastmessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastmessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -20,6 +35,7 @@ socket.on('newMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -32,6 +48,7 @@ socket.on('newLocationMessage', function(message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 $(document).ready(function() {
